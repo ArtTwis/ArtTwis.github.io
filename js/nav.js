93203e795fe88af939af93c0d784f2a09aee7268
Twinkle Goyal <art.twis@gmail.com>
2021-02-15 06:39:54 +0530

    nav links working

--- js/nav.js
+++ js/nav.js
@@ -2,4 +2,9 @@ document.addEventListener('click', (event) => {
   if(event.target.id == 'navIcon'){
     document.querySelector('#navOptions').classList.toggle('show');
   }
+
+  if(event.target.classList.contains('navLink')){
+    document.querySelector('#navOptions').classList.remove('show');
+  }
 })
+
