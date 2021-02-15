2c318f1a40270a4cf5b19b7ba4d8fd64ecd36e0e
Twinkle Goyal <art.twis@gmail.com>
2021-02-15 05:40:45 +0530

    update resume

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('click', (event) => {
   if(event.target.id == 'seeMyResume_btn'){
-    window.open("https://drive.google.com/file/d/1r6AiXU3uwoDYwTBLrIaBxaoOdrmSKv5y/view?usp=sharing", "_blank");
+    window.open("https://drive.google.com/file/d/1NO37YRTVTcVqRgTI0gVtQxf0qClkJYLD/view?usp=sharing", "_blank");
   }
 })
