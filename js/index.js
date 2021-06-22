de7e7f49af3d69d94e4f8978bcd9b180293b2601
Twinkle Goyal <art.twis@gmail.com>
2021-06-23 01:32:57 +0530

    modify resume link

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 document.addEventListener('click', (event) => {
-  if(event.target.id == 'seeMyResume_btn'){
-    window.open("https://drive.google.com/file/d/1NO37YRTVTcVqRgTI0gVtQxf0qClkJYLD/view?usp=sharing", "_blank");
-  }
-})
+	if (event.target.id == 'seeMyResume_btn') {
+		window.open(
+			'https://drive.google.com/file/d/1CclLyCtYFJyr9No6loMJW15WBZxDr9IJ/view?usp=sharing',
+			'_blank'
+		);
+	}
+});
