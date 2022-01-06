89d1feadb1d201a2698cc6c63a8bf280ddd41031
Art_Twis <34947496+ArtTwis@users.noreply.github.com>
2022-01-06 21:28:23 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('click', (event) => {
 	if (event.target.id == 'seeMyResume_btn') {
 		window.open(
-			'https://drive.google.com/file/d/1CclLyCtYFJyr9No6loMJW15WBZxDr9IJ/view?usp=sharing',
+			'https://drive.google.com/file/d/1z2ixFjjHmC1jUIPiZvWUtFrywCeUUXV2/view?usp=drivesdk',
 			'_blank'
 		);
 	}
