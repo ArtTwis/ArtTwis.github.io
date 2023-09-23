04c7b18b7621531112405496dbb1de9eae4b3247
twinkle-goyal <twinkle.goyal@hp.com>
2023-09-24 00:23:31 +0530

    update resume link

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('click', (event) => {
 	if (event.target.id == 'seeMyResume_btn') {
 		window.open(
-			'https://drive.google.com/file/d/1z2ixFjjHmC1jUIPiZvWUtFrywCeUUXV2/view?usp=drivesdk',
+			'https://drive.google.com/file/d/1iezNcBouirRiE4w7R8tk3pwTpHF2I1ZX/view?usp=sharing',
 			'_blank'
 		);
 	}
